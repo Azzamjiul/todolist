2f3eba5f5da1aece7c31e6ef66fabd13b996fb9e
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2023-02-13 22:25:37 +0700

    registration

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class RegisterController extends Controller
+{
+    // menampilkan halaman register
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    // memproses registration
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Session::flash('message', 'Register Berhasil. Akun Anda sudah Aktif silahkan Login menggunakan username dan password.');
+
+        return redirect('login');
+    }
+}
